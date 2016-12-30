5128ee0981dc6b064504f45df31ef8bc94c71f4e
Yvain <yvaindeviragh@gmail.com>
2016-12-30 12:07:50 +0100

    processFrame: Details

--- processFrame.m
+++ processFrame.m
@@ -1,12 +1,13 @@
-function [current_state, current_pose] = processFrame(previous_state, previous_image, current_image )
+function [current_state] = processFrame(previous_state, previous_image, current_image)
 %PROCESSFRAME Processes a frame
 %   Inputs:
 %   - previous_state: Struct with fields
 %       * landmarks: 4xN (homogeneous coordinates)
 %       * keypoints: 2xN
-%       * keypoints_nl: 2xM
-%       * keypoints_nl_1: 2xM
-%       * pose_nl_1: 12xM
+%       * candidate_keypoints: 2xM
+%       * candidate_keypoints_1: 2xM
+%       * candidate_pose_1: 12xM
+%       * pose: 12x1
 %   - previous_image: XxY
 %   - current_image: XxY
 %
@@ -15,7 +16,7 @@ function [current_state, current_pose] = processFrame(previous_state, previous_i
 %   - current_pose: 4x3 non-homogenous matrix representing pose [R|T]
 %   
 %   Internal variables:
-%   - discard: 1xN (array of logicals, where true denotes "discard")
+%   - discard: 1xN (voting array)
 
 %% Setup
 
@@ -24,8 +25,8 @@ discard = zeros(1,N);
 
 %% Apply KLT on current_image
 
-[current_keypoints,discard] = keypointTracking(previous_state.keypoints,...
-    previous_image,current_image,discard);
+[current_keypoints,current_candidate_keypoints,discard] = keypointTracking(previous_state.keypoints,...
+    previous_state.candidate_keypoints,previous_image,current_image,discard);
 
 %% Apply P3P + RANSAC on keypoints with an associated landmark
 
@@ -34,21 +35,30 @@ discard = zeros(1,N);
 
 %% Apply linear triangulation on keypoints without associated landmark
 
-[new_keypoints,new_landmarks,current_keypoints_nl] = triangulation(...
-    current_pose,previous_state.pose_nl_1,current_keypoints,...
-    previous_state.keypoints_nl,previous_state.keypoints_nl_1,discard);
+[new_keypoints,new_landmarks,updated_candidate_keypoints,...
+    updated_candidate_keypoints_1,updated_candidate_pose_1] = triangulation(...
+    current_pose,previous_state.candidate_pose_1,current_keypoints,...
+    current_candidate_keypoints,previous_state.candidate_keypoints_1);
+
 
 %% Find new features
 %  - Perform suppression around existing features?
 %  - How about FAST instaed of Harris?
 %  - Do this in every iteration or only once in a while?
+%  - Variable "discard" really needed?
+%  - Note: new_candidate_keypoints = new_candidate_keypoints_1!
 
-[new_keypoints_nl_1] = featureExtraction(current_image,current_keypoints,current_keypoints_nl,discard);
+[new_candidate_keypoints,new_candidate_keypoints_1,new_candidate_pose_1] = featureExtraction(...
+    current_image,current_keypoints,new_keypoints,updated_candidate_keypoints,discard);
 
 %% What is left to do
-%  - Stitch together current_state.
+%  - Kick out old/bad candidate_keypoints
 
-
+current_state.landmarks = [current_landmarks new_landmarks]; % TODO: Applly discard on current_landmarks
+current_state.keypoints = [current_keypoints new_keypoints]; % TODO: Apply discard on current_keypoints
+current_state.candidate_keypoints = [updated_candidate_keypoints new_candidate_keypoints];
+current_state.candidate_keypoints_1 = [updated_candidate_keypoints_1 new_candidate_keypoints_1];
+current_state.candidate_pose_1 = [updated_candidate_pose_1 new_candidate_pose_1];
 
 end
 
