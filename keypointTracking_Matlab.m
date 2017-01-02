d92d6095c455299e6e9a2c0efb214af2915e454c
ochsnermarc <ochsnerm@student.ethz.ch>
2017-01-02 18:03:39 +0100

    Discard from negative values to inf

--- keypointTracking_Matlab.m
+++ keypointTracking_Matlab.m
@@ -30,7 +30,7 @@ initialize(pointTracker,points,previous_image);
 % discard = discard.*(point_validity)';
 
 % Delete points which are not in the image anymore
-% new_points(new_points<0) = 0;
+discard(new_points(:,1)<0 | new_points(:,2)<0)  = inf;
 % new_points(new_points(:,1)>width,:) = 0;
 % new_points(new_points(:,2)>height,:) = 0;
 
@@ -45,6 +45,7 @@ if ~isempty(previous_state_candidate_keypoints)
     initialize(pointTracker,candidate_points,previous_image);
 
     [new_candidate_points,candidate_point_validity] = step(pointTracker,current_image);
+    candidate_discard(new_candidate_points(:,1)<0 | new_candidate_points(:,2)<0)  = inf;
     candidate_discard(~candidate_point_validity) = inf;
 %      current_candidate_keypoints = new_candidate_points.*candidate_point_validity;
     current_candidate_keypoints = (new_candidate_points)';
