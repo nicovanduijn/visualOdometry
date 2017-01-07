3b95ef81bf27908cfb8a43b74f5764b587088c2f
ochsnermarc <ochsnerm@student.ethz.ch>
2017-01-07 13:25:25 +0100

    Comments in KeypointTracking

--- approvedFunctions/keypointTracking.m
+++ approvedFunctions/keypointTracking.m
@@ -21,15 +21,20 @@ min_dist_edge_of_the_screen = params.track_min_kp_dist_to_edge;
 %% Keypoints tracking
 points = (previous_state_keypoints)';
 
- width = size(previous_image,2);
- height = size(previous_image,1);
+%Set width and height of the frame
+width = size(previous_image,2);
+height = size(previous_image,1);
 
+%Configure the tracker object properties
 pointTracker = vision.PointTracker;
+
+%Initialize Tracking Process
 initialize(pointTracker,points,previous_image);
 
+%Track keypoints in new frame
 [new_points,point_validity] = step(pointTracker,current_image);
 
-% Delete points which are not in the image anymore
+% Set discard of points which are not in the image anymore to infinity
 discard(new_points(:,1)<min_dist_edge_of_the_screen | new_points(:,2)<min_dist_edge_of_the_screen)  = inf;
 discard(new_points(:,2)>(height-min_dist_edge_of_the_screen) | new_points(:,1)>(width-min_dist_edge_of_the_screen)) = inf;
 discard(~point_validity) = inf;
@@ -38,18 +43,26 @@ discard(~point_validity) = inf;
 if ~isempty(previous_state_candidate_keypoints)
     
     candidate_points = (previous_state_candidate_keypoints)';
+    
+    %Configure the tracker object properties
     pointTracker = vision.PointTracker;
+    
+    %Initialize Tracking Process
     initialize(pointTracker,candidate_points,previous_image);
-
+    
+    %Track keypoints in new frame
     [new_candidate_points,candidate_point_validity] = step(pointTracker,current_image);
     candidate_discard(new_candidate_points(:,1)<0 | new_candidate_points(:,2)<0)  = inf;
     candidate_discard(new_candidate_points(:,2)>height | new_candidate_points(:,1)>width)  = inf;
+    
+    % Set discard of points which are not in the image anymore to infinity
     candidate_discard(~candidate_point_validity) = inf;
     current_candidate_keypoints = (new_candidate_points)';
 else
     current_candidate_keypoints = previous_state_candidate_keypoints;
     
 end
+
 
 current_keypoints = (new_points)';
 
