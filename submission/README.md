57a04492e76dc240d8109c04c26bdd8b0e67df69
nicovanduijn <vanduijn@gatech.edu>
2017-01-07 13:01:32 +0100

    submission folder removed

--- submission/README.md
+++
@@ -1,5 +0,0 @@
-# visualOdometry
-name conventions here
-
-state: struct
- - state.landmarks (4xN matrix with homogenous points in world frame)
--- submission/approvedFunctions/deHomogenize.m
+++
@@ -1,7 +0,0 @@
-function [ output] = deHomogenize( input )
-%DEHOMOGENIZE de-homogenizes n m-dimensional points given as nxm matrix
-%   Detailed explanation goes here
-
-output = input(1:end-1, :);
-end
-
